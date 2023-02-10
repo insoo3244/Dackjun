515593e268f1ddfe08f7279602cb30ee2caff4e1
insoo3244 <insoo_3244@naver.com>
2023-02-10 20:57:32 +0900

    230210_2

---
+++ 51-100/10833.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+    int sum = 0;
+    while(n--){
+        int a;
+        int b;
+        cin >> a >> b;
+        sum += b % a;
+    }
+
+    cout << sum;
+}
